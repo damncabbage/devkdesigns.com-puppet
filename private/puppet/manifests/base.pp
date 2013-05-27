e6adc48c9162f570d0683cc2ca86f9e281d2c100
Rob Howard <rob@robhoward.id.au>
2013-05-27 23:41:00 +1000

    Swaps out default nginx site for the preliminary devkdesigns.com one.

--- private/puppet/manifests/base.pp
+++ private/puppet/manifests/base.pp
@@ -2,18 +2,15 @@ group { 'puppet':
 	ensure => present,
 }
 
-exec { 'apt-get update': 
+exec { 'apt-get update':
 	command => '/usr/bin/apt-get update',
 }
 
-package { 'nginx': 
-	ensure => present,
-	require => Exec['apt-get update'],
-}
+Package { require => Exec['apt-get update'] }
 
-package { 'php5-fpm':
+# Services
+package { 'nginx':
 	ensure => present,
-	require => Exec['apt-get update'],
 }
 
 service { 'nginx':
@@ -21,16 +18,34 @@ service { 'nginx':
 	require => Package['nginx'],
 }
 
+package { 'php5-fpm':
+	ensure => present,
+}
+
 service { 'php5-fpm':
 	ensure => running,
 	require => Package['php5-fpm'],
 }
 
-file { 'vagrant-nginx':
-	path => '/etc/nginx/sites-available/vagrant',
+package { 'mysql-server':
+	ensure => present,
+}
+
+service { 'mysql':
+	ensure => running,
+	require => Package['mysql-server'],
+}
+
+# Tools and Libraries
+$libraries = [ "mysql-client", "php5-gd", "php5-mysql", "php5-curl" ]
+package { $libraries: ensure => present }
+
+# Config Files
+file { 'devkdesigns-nginx':
+	path => '/etc/nginx/sites-available/devkdesigns',
 	ensure => file,
 	require => Package['nginx'],
-	source => 'puppet:///modules/nginx/vagrant',
+	source => 'puppet:///modules/nginx/devkdesigns',
 }
 
 file { 'default-nginx-disable':
@@ -39,13 +54,13 @@ file { 'default-nginx-disable':
 	require => Package['nginx'],
 }
 
-file { 'vagrant-nginx-enable':
-	path => '/etc/nginx/sites-enabled/vagrant',
-	target => '/etc/nginx/sites-available/vagrant',
+file { 'devkdesigns-nginx-enable':
+	path => '/etc/nginx/sites-enabled/devkdesigns',
+	target => '/etc/nginx/sites-available/devkdesigns',
 	ensure => link,
 	notify => Service['nginx'],
 	require => [
-		File['vagrant-nginx'],
+		File['devkdesigns-nginx'],
 		File['default-nginx-disable'],
 	],
 }
